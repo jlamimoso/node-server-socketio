250cd314ee22de704a179ec63912e3eb56e5373b
jlamimoso <jlamimoso@Jose-Luiz-Alla-Mimosos-MacBook-Pro.local>
2021-01-28 13:02:00 -0300

    ajuste porta

--- main.js
+++ main.js
@@ -66,6 +66,7 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => {
+var port = process.env.PORT || 3000;
+http.listen(port, () => {
     console.log('listening on *:3000');
 });
